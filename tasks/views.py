3215f22d47e3b6069c58a10c3a2f2be073655238
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-09-14 16:48:14 +0200

    mise en place retourner un composant html

--- tasks/views.py
+++ tasks/views.py
@@ -41,4 +41,5 @@ def add_task(request):
 def get_tasks(request, collection_pk):
     collection = get_object_or_404(Collection, pk=collection_pk)
     tasks = collection.task_set.order_by("description")
+
     return HttpResponse("<br>".join(task.description for task in tasks ))
